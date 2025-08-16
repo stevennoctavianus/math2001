1b431d2c83224edefd8881e9695c485931479bea
stevennoctavianus <stevenoctavianus11@gmail.com>
2025-08-16 15:14:00 +0800

    chap01

--- Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
+++ Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
@@ -12,43 +12,77 @@ proofs. -/
 
 -- Example 1.3.1
 example {a b : ℤ} (h1 : a = 2 * b + 5) (h2 : b = 3) : a = 11 :=
-  sorry
+  calc
+    a = 2 * b + 5 := by rw [h1]
+    _ = 2 * 3 + 5 := by rw [h2]
+    _ = 11 := by ring
 
 -- Example 1.3.2
 example {x : ℤ} (h1 : x + 4 = 2) : x = -2 :=
-  sorry
+  calc
+      x = (x + 4) - 4 := by ring
+      _ = 2 - 4 := by rw[h1]
+      _ = -2 := by ring
 
 -- Example 1.3.3
 example {a b : ℝ} (h1 : a - 5 * b = 4) (h2 : b + 2 = 3) : a = 9 :=
-  sorry
+  calc
+    a = (a - 5*b) + 5*b := by ring
+    _ = 4 + 5*b := by rw[h1]
+    _ = 4 + 5*((b + 2) - 2) := by ring
+    _ = 4 + 5*(3 - 2) := by rw[h2]
+    _ = 9 := by ring
 
 -- Example 1.3.4
 example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 :=
-  sorry
+  calc
+    w = (3*w + 1)/3 - 1/3 := by ring
+    _ = 4/3 - 1/3 := by rw[h1]
+    _ = 1 := by ring
 
 -- Example 1.3.5
 example {x : ℤ} (h1 : 2 * x + 3 = x) : x = -3 :=
-  sorry
+  calc
+    x = (2*x + 3) - x - 3 := by ring
+    _ = x - x - 3 := by rw[h1]
+    _ = -3 := by ring
 
 -- Example 1.3.6
 example {x y : ℤ} (h1 : 2 * x - y = 4) (h2 : y - x + 1 = 2) : x = 5 :=
-  sorry
+  calc
+    x = (2*x - y) + (y - x + 1) - 1 := by ring
+    _ = 4 + 2 - 1 := by rw[h1, h2]
+    _ = 5 := by ring
 
 -- Example 1.3.7
 example {u v : ℚ} (h1 : u + 2 * v = 4) (h2 : u - 2 * v = 6) : u = 5 :=
-  sorry
+  calc
+    u = 1/2 * ((u + 2*v) + (u - 2*v)) := by ring
+    _ = 1/2 * (4 + 6) := by rw[h1, h2]
+    _ = 5 := by ring
 
 -- Example 1.3.8
 example {x y : ℝ} (h1 : x + y = 4) (h2 : 5 * x - 3 * y = 4) : x = 2 :=
-  sorry
+  calc
+    x = 1/8 * (3*(x + y) + (5*x - 3*y)) := by ring
+    _ = 1/8 * (3 * 4 + 4) := by rw[h1, h2]
+    _ = 2 := by ring
 
 -- Example 1.3.9
 example {a b : ℚ} (h1 : a - 3 = 2 * b) : a ^ 2 - a + 3 = 4 * b ^ 2 + 10 * b + 9 :=
-  sorry
+  calc
+    a^2 - a + 3
+    _ = (a - 3)^2 + 5*(a - 3) + 9 := by ring
+    _ = (2*b)^2 + 5*(2*b) + 9 := by rw[h1]
+    _ = 4*b^2 + 10*b + 9 := by ring
 
 -- Example 1.3.10
 example {z : ℝ} (h1 : z ^ 2 - 2 = 0) : z ^ 4 - z ^ 3 - z ^ 2 + 2 * z + 1 = 3 :=
-  sorry
+  calc
+    z^4 - z^3 - z^2 + 2*z + 1
+      = (z^2 - 2)*(z^2 - z + 1) + 3 := by ring
+    _ = 0 * (z^2 - z + 1) + 3 := by rw[h1]
+    _ = 3 := by ring
 
 /-! # Exercises
 
@@ -56,12 +90,13 @@ Solve these problems yourself.  You may find it helpful to solve them on paper b
 up in Lean. -/
 
 
-example {x y : ℝ} (h1 : x = 3) (h2 : y = 4 * x - 3) : y = 9 :=
-  sorry
+-- example {x y : ℝ} (h1 : x = 3) (h2 : y = 4 * x - 3) : y = 9 :=
+---  sorry
 
-example {a b : ℤ} (h : a - b = 0) : a = b :=
-  sorry
+--- example {a b : ℤ} (h : a - b = 0) : a = b :=
+---  sorry
 
+/-
 example {x y : ℤ} (h1 : x - 3 * y = 5) (h2 : y = 3) : x = 14 :=
   sorry
 
@@ -93,16 +128,29 @@ example {x y : ℤ} (h1 : 2 * x + y = 4) (h2 : x + y = 1) : x = 3 :=
 example {a b : ℝ} (h1 : a + 2 * b = 4) (h2 : a - b = 1) : a = 2 :=
   sorry
 
+    -/
+
 example {u v : ℝ} (h1 : u + 1 = v) : u ^ 2 + 3 * u + 1 = v ^ 2 + v - 1 :=
-  sorry
+  calc
+      u^2 + 3*u + 1
+      = (u + 1)^2 + (u + 1) - 1 := by ring
+    _ = v^2 + v - 1 := by rw[h1]
 
 example {t : ℚ} (ht : t ^ 2 - 4 = 0) :
     t ^ 4 + 3 * t ^ 3 - 3 * t ^ 2 - 2 * t - 2 = 10 * t + 2 :=
-  sorry
+  calc
+      t ^ 4 + 3 * t ^ 3 - 3 * t ^ 2 - 2 * t - 2
+      = (t^2 - 4)*(t^2 + 3*t + 1) + 10*t + 2 := by ring
+    _ = 0 * (t^2 + 3*t + 1) + 10*t + 2 := by rw[ht]
+    _ = 10*t + 2 := by ring
 
-example {x y : ℝ} (h1 : x + 3 = 5) (h2 : 2 * x - y * x = 0) : y = 2 :=
-  sorry
+
+--- example {x y : ℝ} (h1 : x + 3 = 5) (h2 : 2 * x - y * x = 0) : y = 2 :=
 
 example {p q r : ℚ} (h1 : p + q + r = 0) (h2 : p * q + p * r + q * r = 2) :
     p ^ 2 + q ^ 2 + r ^ 2 = -4 :=
-  sorry
+  calc
+      p^2 + q^2 + r^2
+      = (p + q + r)^2 - 2*(p*q + p*r + q*r) := by ring
+    _ = 0^2 - 2*(2) := by rw[h1, h2]
+    _ = -4 := by ring
